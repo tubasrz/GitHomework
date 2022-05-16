089f2a71774e4318232f43167a6ca6074ded3453
sadettin uysal <tubasariz@gmail.com>
2022-05-15 22:23:44 -0400

    first commit

---
+++ src/com/company/Homework.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Homework {
+
+    public static void main(String[] args) {
+
+        System.out.println("Homework 1");
+
+        for(int i=0; i<=100; i++){
+            System.out.println(i +" ");
+        }
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        //write your code here
+        System.out.println("I am Groot!!");
+        System.out.println("I am Groot2!!");
+        System.out.println("I am Groot3!!");
+
+        System.out.println("I am correct code");
+
+    }
+}
